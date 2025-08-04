450b153d8a606130a361f9ad55f2c3cf96bf3586
Totan Patra <totanpatra459@gmail.com>
2025-08-04 17:04:37 +0530

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Warehouse Dispatcher Recorder',
+  description: 'Created with Chandan',
+  generator: 'Cahnadn&Totan',
 }
 
 export default function RootLayout({
